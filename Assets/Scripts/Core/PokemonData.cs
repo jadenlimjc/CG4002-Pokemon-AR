c86878380b7c3deabfbeda96f3198d9dee1b4278
Jaden Lim <jadenlimjc@gmail.com>
2026-09-03 14:29:19 +0800

    Fix: Added None as option for secondary pokemon type

--- Assets/Scripts/Core/PokemonData.cs
+++ Assets/Scripts/Core/PokemonData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [Serializable]
 public enum PokemonType
 {
+    None,
     Normal, Fire, Water, Grass, Electric, Ice,
     Fighting, Poison, Ground, Flying, Psychic,
     Bug, Rock, Ghost, Dragon, Dark, Steel, Fairy
